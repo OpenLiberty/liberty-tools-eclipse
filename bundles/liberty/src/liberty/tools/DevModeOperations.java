26537926818b8454a29df8b93170fcc16f02292e
Edward Mezarina <mezarina@us.ibm.com>
2022-03-16 15:51:02 -0400

    Prototype updates to fully support running all required devMode actions on a TM terminal.
    Signed-off-by: Edward Mezarina <mezarina@us.ibm.com>

--- bundles/liberty/src/liberty/tools/DevModeOperations.java
+++ bundles/liberty/src/liberty/tools/DevModeOperations.java
@@ -1,5 +1,6 @@
 package liberty.tools;
 
+import java.io.OutputStream;
 import java.net.URL;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -9,13 +10,24 @@ import java.util.List;
 import java.util.Map;
 
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.jface.dialogs.IInputValidator;
+import org.eclipse.jface.dialogs.InputDialog;
+import org.eclipse.jface.window.Window;
+import org.eclipse.swt.custom.CTabItem;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.tm.internal.terminal.provisional.api.ITerminalConnector;
 import org.eclipse.tm.terminal.view.core.TerminalServiceFactory;
 import org.eclipse.tm.terminal.view.core.interfaces.ITerminalService;
 import org.eclipse.tm.terminal.view.core.interfaces.constants.ITerminalsConnectorConstants;
+import org.eclipse.tm.terminal.view.ui.manager.ConsoleManager;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.browser.IWebBrowser;
 import org.eclipse.ui.browser.IWorkbenchBrowserSupport;
 
+import liberty.tools.ui.terminal.LocalDevModeLauncherDelegate;
+import liberty.tools.ui.terminal.TerminalTabListenerImpl;
 import liberty.tools.utils.Dialog;
 import liberty.tools.utils.Project;
 
@@ -23,6 +35,7 @@ import liberty.tools.utils.Project;
  * Provides the implementation of all supported dev mode operations.
  */
 public class DevModeOperations {
+
     // TODO: Dashboard display: Handle the case where the project is configured to be built/run by both
     // Gradle and Maven at the same time.
 
@@ -42,6 +55,11 @@ public class DevModeOperations {
         IProject project = Project.getSelected();
         String projectName = project.getName();
 
+        // Check if the application has already been started.
+        if (isStarted(projectName)) {
+            return;
+        }
+
         try {
             // Get the absolute path to the application project.
             String projectPath = Project.getPath(project);
@@ -53,8 +71,9 @@ public class DevModeOperations {
             String cmd = "";
             if (Project.isMaven(project)) {
                 cmd = getMavenCommand("io.openliberty.tools:liberty-maven-plugin:dev -f " + projectPath);
+
             } else if (Project.isGradle(project)) {
-                cmd = getGradleCommand("gradle libertyDev -p=" + projectPath);
+                cmd = getGradleCommand("libertyDev -p=" + projectPath);
             } else {
                 Dialog.displayErrorMessage("Project" + projectName + "is not a Gradle or Maven project.");
 
@@ -75,11 +94,23 @@ public class DevModeOperations {
      * 
      * @return An error message or null if the command was processed successfully.
      */
-    public void startWithParms(String userParms) {
+    public void startWithParms() {
         IProject project = Project.getSelected();
         String projectName = project.getName();
 
+        // Check if the application has already been started.
+        if (isStarted(projectName)) {
+            return;
+        }
+
         try {
+            // Get start parameters from the user. If the user cancelled or closed the parameter dialog,
+            // take that as indication that no action should take place.
+            String userParms = getStartParms();
+            if (userParms == null) {
+                return;
+            }
+
             // Get the absolute path to the application project.
             String projectPath = Project.getPath(project);
             if (projectPath == null) {
@@ -91,7 +122,7 @@ public class DevModeOperations {
             if (Project.isMaven(project)) {
                 cmd = getMavenCommand("io.openliberty.tools:liberty-maven-plugin:dev " + userParms + " -f " + projectPath);
             } else if (Project.isGradle(project)) {
-                cmd = getGradleCommand("gradle libertyDev " + userParms + " -p=" + projectPath);
+                cmd = getGradleCommand("libertyDev " + userParms + " -p=" + projectPath);
             } else {
                 Dialog.displayErrorMessage("Project" + projectName + "is not a Gradle or Maven project.");
                 return;
@@ -115,6 +146,11 @@ public class DevModeOperations {
         IProject project = Project.getSelected();
         String projectName = project.getName();
 
+        // Check if the application has already been started.
+        if (isStarted(projectName)) {
+            return;
+        }
+
         try {
             // Get the absolute path to the application project.
             String projectPath = Project.getPath(project);
@@ -127,7 +163,7 @@ public class DevModeOperations {
             if (Project.isMaven(project)) {
                 cmd = getMavenCommand("io.openliberty.tools:liberty-maven-plugin:devc -f " + projectPath);
             } else if (Project.isGradle(project)) {
-                cmd = getGradleCommand("gradle libertyDevc -p=" + projectPath);
+                cmd = getGradleCommand("libertyDevc -p=" + projectPath);
             } else {
                 Dialog.displayErrorMessage("Project" + projectName + "is not a Gradle or Maven project.");
             }
@@ -151,10 +187,31 @@ public class DevModeOperations {
         String projectName = project.getName();
 
         try {
-            String cmd = "q";
-            runCommand(cmd, projectName);
+            // Validate that we can get to the respective terminal's output stream.
+            LocalDevModeLauncherDelegate delegate = LocalDevModeLauncherDelegate.getInstance();
+            if (delegate == null) {
+                throw new Exception("Unable to find the development mode launcher delegate. Be sure to run the start action first.");
+            }
+
+            ITerminalConnector terminalConnector = delegate.getConnector(projectName);
+            if (terminalConnector == null) {
+                throw new Exception(
+                        "Unable to find terminal connector. Be sure to run the start action first. Note that attempting to stop orphaned processes with this action in not valid. Orphaned processes require manual intervention.");
+            }
+
+            OutputStream terminalStream = terminalConnector.getTerminalToRemoteStream();
+            if (terminalStream == null) {
+                throw new Exception(
+                        "Unable to find terminal remote stream. The terminal might not be active. Be sure to run the start action first.  Note that attempting to stop orphaned processes with this action in not valid. Orphaned processes require manual intervention.");
+            }
+
+            // Prepare the development mode command to stop the server.
+            String cmd = "exit" + System.lineSeparator();
+
+            // Issue the command on the terminal.
+            terminalStream.write(cmd.getBytes());
         } catch (Exception e) {
-            Dialog.displayErrorMessageWithDetails("An error was detected while performing the stop action.", e);
+            Dialog.displayErrorMessageWithDetails("An error was detected while performing the stop action on project " + projectName, e);
             return;
         }
     }
@@ -167,11 +224,33 @@ public class DevModeOperations {
     public void runTests() {
         IProject project = Project.getSelected();
         String projectName = project.getName();
-        String cmd = " ";
+
         try {
-            runCommand(cmd, projectName);
+            // Validate that we can get to the respective terminal's output stream.
+            LocalDevModeLauncherDelegate delegate = LocalDevModeLauncherDelegate.getInstance();
+            if (delegate == null) {
+                throw new Exception("Unable to find the development mode launcher delegate. Be sure to run the start action first.");
+            }
+
+            ITerminalConnector terminalConnector = delegate.getConnector(projectName);
+            if (terminalConnector == null) {
+                throw new Exception("Unable to find terminal connector. Be sure to run the start action first.");
+            }
+
+            OutputStream terminalStream = terminalConnector.getTerminalToRemoteStream();
+            if (terminalStream == null) {
+                throw new Exception(
+                        "Unable to find terminal remote stream. The terminal might not be active. Be sure to run the start action first.");
+            }
+
+            // Prepare the development mode command to run a test.
+            String cmd = System.lineSeparator();
+
+            // Issue the command on the terminal
+            terminalStream.write(cmd.getBytes());
         } catch (Exception e) {
-            Dialog.displayErrorMessageWithDetails("An error was detected while performing the run tests action.", e);
+            Dialog.displayErrorMessageWithDetails("An error was detected while performing the run tests action on project " + projectName,
+                    e);
             return;
         }
     }
@@ -309,7 +388,7 @@ public class DevModeOperations {
         Map<String, Object> properties = new HashMap<String, Object>();
         properties.put(ITerminalsConnectorConstants.PROP_TITLE, projectName);
         properties.put(ITerminalsConnectorConstants.PROP_ENCODING, "UTF-8");
-        properties.put(ITerminalsConnectorConstants.PROP_DELEGATE_ID, "org.eclipse.tm.terminal.connector.local.launcher.local");
+        properties.put(ITerminalsConnectorConstants.PROP_DELEGATE_ID, LocalDevModeLauncherDelegate.id);
         properties.put(ITerminalsConnectorConstants.PROP_PROCESS_ARGS, cmd);
         properties.put(ITerminalsConnectorConstants.PROP_DATA, projectName);
         properties.put(ITerminalsConnectorConstants.PROP_PROCESS_ENVIRONMENT, envs.toArray(new String[envs.size()]));
@@ -318,7 +397,62 @@ public class DevModeOperations {
         properties.put(ITerminalsConnectorConstants.PROP_DATA_NO_RECONNECT, Boolean.TRUE);
 
         ITerminalService ts = TerminalServiceFactory.getService();
-        ts.openConsole(properties, null);
+
+        ITerminalService.Done done = new ITerminalService.Done() {
+            @Override
+            public void done(IStatus status) {
+                if (status.getCode() == IStatus.OK) {
+                    TerminalTabListenerImpl tabListener = new TerminalTabListenerImpl(ts, projectName);
+                    ts.addTerminalTabListener(tabListener);
+                }
+            }
+        };
+
+        ts.openConsole(properties, done);
+    }
+
+    /**
+     * Returns the list of parameters if the user presses OK, null otherwise.
+     * 
+     * @return The list of parameters if the user presses OK, null otherwise.
+     */
+    public String getStartParms() {
+        String dTitle = "Liberty Development Mode";
+        String dMessage = "Specify custom parameters for the liberty dev command.";
+        String dInitValue = "";
+        IInputValidator iValidator = getParmListValidator();
+        Shell shell = Display.getCurrent().getActiveShell();
+        InputDialog iDialog = new InputDialog(shell, dTitle, dMessage, dInitValue, iValidator) {
+        };
+
+        String userInput = null;
+
+        if (iDialog.open() == Window.OK) {
+            userInput = iDialog.getValue().trim();
+        }
+
+        return userInput;
+    }
+
+    /**
+     * Creates a validation object for user provided parameters.
+     * 
+     * @return A validation object for user provided parameters.
+     */
+    public IInputValidator getParmListValidator() {
+        return new IInputValidator() {
+
+            @Override
+            public String isValid(String text) {
+                String[] parmSegments = text.split(" ");
+                for (int i = 0; i < parmSegments.length; i++) {
+                    if (parmSegments[i] != null && !parmSegments[i].isEmpty() && !parmSegments[i].startsWith("-")) {
+                        return "Parameters must start with -";
+                    }
+                }
+                return null;
+            }
+        };
     }
 
     /**
@@ -381,13 +515,13 @@ public class DevModeOperations {
     /**
      * Returns the full Maven command to run on the terminal.
      * 
-     * @param baseCommand The mvn command args
+     * @param cmdArgs The mvn command args
      * 
      * @return The full Maven command to run on the terminal.
      */
     private String getMavenCommand(String cmdArgs) {
         StringBuilder sb = new StringBuilder();
-        
+
         String baseCmd = isWindows() ? "mvn.cmd" : "mvn";
         String mvnCmd = null;
 
@@ -399,30 +533,43 @@ public class DevModeOperations {
         }
 
         sb.append(mvnCmd).append(" ").append(cmdArgs);
-        
+
         if (isWindows()) {
             // Include trailing space for separation
             sb.insert(0, "/c ");
-        } 
-        
+        }
+
         return sb.toString();
     }
 
     /**
      * Returns the full Gradle command to run on the terminal.
      * 
-     * @param baseCommand The base development mode command.
+     * @param cmdArgs The Gradle command args.
      * 
      * @return The full Gradle command to run on the terminal.
      */
-    private String getGradleCommand(String baseCommand) {
-        StringBuilder cmd = new StringBuilder(baseCommand);
+    private String getGradleCommand(String cmdArgs) {
+        StringBuilder sb = new StringBuilder();
+
+        String baseCmd = isWindows() ? "gradle.bat" : "gradle";
+        String gradleCmd = null;
+
         String gradleInstallPath = getGradleInstallHome();
         if (gradleInstallPath != null) {
-            return Paths.get(gradleInstallPath, "bin", cmd.toString()).toString();
+            gradleCmd = Paths.get(gradleInstallPath, "bin", baseCmd).toString();
+        } else {
+            gradleCmd = baseCmd;
         }
 
-        return cmd.insert(0, "gradle ").toString();
+        sb.append(gradleCmd).append(" ").append(cmdArgs);
+
+        if (isWindows()) {
+            // Include trailing space for separation
+            sb.insert(0, "/c ");
+        }
+
+        return sb.toString();
     }
 
     /**
@@ -440,5 +587,39 @@ public class DevModeOperations {
         Path path = Paths.get(projectPath, "build", "reports", "tests", "test", "index.html");
 
         return path;
+    }
+
+    /**
+     * Returns true if the input project has already been started. False, otherwise.
+     * 
+     * @param projectName The project name to check.
+     * 
+     * @return True if the input project has already been started. False, otherwise
+     */
+    private boolean isStarted(String projectName) {
+        // Find if there is a connector already associated with the project. If there is one, make sure
+        // that associated terminal was terminated.
+        LocalDevModeLauncherDelegate delegate = LocalDevModeLauncherDelegate.getInstance();
+        if (delegate != null) {
+            ITerminalConnector connector = delegate.getConnector(projectName);
+
+            if (connector != null) {
+                ConsoleManager consoleMgr = ConsoleManager.getInstance();
+                CTabItem item = consoleMgr.findConsole(null, null, projectName, connector, null);
+                if (item != null) {
+                    if (!item.getText().contains("<Closed>")) {
+                        Dialog.displayWarningMessage("Application project " + projectName + " is already running.");
+                        return true;
+                    } else {
+                        // There is no easy way to get notified when the terminal is disconnected, so proactively close the
+                        // terminal as it would on normal restart (PROP_FORCE_NEW=TRUE) in order to cleanup. This action
+                        // triggers the registered tab listeners to be called.
+                        item.dispose();
+                    }
+                }
+            }
+        }
+
+        return false;
     }
 }
