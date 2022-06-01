d4f84dc6aad67c5ab7b48ac40512ec2411f7c7b5
Scott Kurz <skurz@us.ibm.com>
2022-06-01 18:13:32 -0400

    Add Java copyrights
    Signed-off-by: Scott Kurz <skurz@us.ibm.com>

--- tests/resources/applications/maven/liberty-maven-test-app/src/test/java/test/maven/liberty/web/app/it/EndpointIT.java
+++ tests/resources/applications/maven/liberty-maven-test-app/src/test/java/test/maven/liberty/web/app/it/EndpointIT.java
@@ -1,18 +1,15 @@
 /*******************************************************************************
- * (c) Copyright IBM Corporation 2017.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *******************************************************************************/
+* Copyright (c) 2022 IBM Corporation and others.
+*
+* This program and the accompanying materials are made available under the
+* terms of the Eclipse Public License v. 2.0 which is available at
+* http://www.eclipse.org/legal/epl-2.0.
+*
+* SPDX-License-Identifier: EPL-2.0
+*
+* Contributors:
+*     IBM Corporation - initial implementation
+*******************************************************************************/
 package test.maven.liberty.web.app.it;
 
 import static org.junit.Assert.*;
