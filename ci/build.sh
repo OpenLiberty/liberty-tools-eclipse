f0058e44b49be12cc77bc985fb52e28cded36376
Adam Wisniewski <awisniew@us.ibm.com>
2022-09-14 20:48:49 -0400

    Copy artifacts.jar
    Signed-off-by: Adam Wisniewski <awisniew@us.ibm.com>

--- ci/build.sh
+++ ci/build.sh
@@ -4,7 +4,7 @@ set -Eexo pipefail
 
 DOCKER_WORK_DIR="eclipse-ltp"
 DOCKER_REL_REPO_DIR_LOC="/liberty-tools-eclipse/releng/io.openliberty.tools.update/target/repository"
-DOCKER_REL_ZIP_LOC="/liberty-tools-eclipse/releng/io.openliberty.tools.update/target/io.openliberty.tools.update.eclipse-repository-0.1.1.zip"
+DOCKER_REL_ZIP_LOC="/liberty-tools-eclipse/releng/io.openliberty.tools.update/target/repo_zip"
 DOCKER_DOMAIN="release"
 DOCKER_IMG_NAME="tmp-build-image-liberty-tools-eclipse:0.1.1"
 DOCKER_IMG="${DOCKER_DOMAIN}/${DOCKER_IMG_NAME}"
@@ -24,7 +24,7 @@ container_id=$(docker create "$DOCKER_IMG")
 
 # Copy the release artifacts locally for signing.
 docker cp "${container_id}:/${DOCKER_WORK_DIR}/${DOCKER_REL_REPO_DIR_LOC}/" "$RELEASE_OUTPUT_DIR"
-docker cp "${container_id}:/${DOCKER_WORK_DIR}/${DOCKER_REL_ZIP_LOC}" "$RELEASE_OUTPUT_DIR"
+docker cp "${container_id}:/${DOCKER_WORK_DIR}/${DOCKER_REL_ZIP_LOC}/." "$RELEASE_OUTPUT_DIR"
 
 # Delete the container.
 docker rm -v "$container_id"
