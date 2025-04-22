52414965bfe693d198663ccfb87e13e4bf42e773
Aparna Michael <Aparna.Michael@ibm.com>
2025-04-22 22:12:58 +0530

    Re-enabled app monitoring if the user disconnects the debug mode.

--- bundles/io.openliberty.tools.eclipse.ui/src/io/openliberty/tools/eclipse/process/LibertyDebugEventListener.java
+++ bundles/io.openliberty.tools.eclipse.ui/src/io/openliberty/tools/eclipse/process/LibertyDebugEventListener.java
@@ -3,6 +3,8 @@ package io.openliberty.tools.eclipse.process;
 import org.eclipse.debug.core.DebugEvent;
 import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.IDebugEventSetListener;
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.core.model.IDebugTarget;
 import org.eclipse.debug.core.model.IProcess;
 
 import io.openliberty.tools.eclipse.DevModeOperations;
@@ -21,6 +23,7 @@ public class LibertyDebugEventListener implements IDebugEventSetListener {
     public void handleDebugEvents(DebugEvent[] events) {
         for (int i = 0; i < events.length; i++) {
             Object source = events[i].getSource();
+            DevModeOperations devModeOps = DevModeOperations.getInstance();
 
             if (source instanceof IProcess && events[i].getKind() == DebugEvent.TERMINATE) {
 
@@ -28,7 +31,6 @@ public class LibertyDebugEventListener implements IDebugEventSetListener {
                 IProcess iProcess = (IProcess) source;
                 if (projectName.equals(iProcess.getLabel())) {
                     // We match - cleanup
-                    DevModeOperations devModeOps = DevModeOperations.getInstance();
                     Project project = devModeOps.getProjectModel().getProject(projectName);
                     
                     if (project != null) {
@@ -37,7 +39,24 @@ public class LibertyDebugEventListener implements IDebugEventSetListener {
                     devModeOps.cleanupProcess(projectName);
                     DebugPlugin.getDefault().removeDebugEventListener(this);
                 }
-            }
+            } else if (events[i].getKind() == DebugEvent.CHANGE && source instanceof IDebugTarget) {
+    			IDebugTarget target = (IDebugTarget) source;
+    			if (target.isDisconnected()) {
+    				ILaunch launch = target.getLaunch();
+    				if (launch != null) {
+    					IProcess[] processes = launch.getProcesses();
+    					if (processes.length > 0) {
+    						String label = processes[0].getLabel(); 
+    						if (projectName.equals(label)) {
+    							Project project = devModeOps.getProjectModel().getProject(projectName);
+    							if (project != null) {
+    								Utils.reEnableAppMonitoring(project);
+    							}
+    						}
+    					}
+    				}
+    			}                  
+    		}
         }
     }
 
