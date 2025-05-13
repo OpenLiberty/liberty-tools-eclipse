f218081a4fcc321f6b17cad4be3e62971db70891
SuparnaSuresh <suparna.suresh@ibm.com>
2025-05-13 14:10:40 +0530

    Removed unused imports

--- tests/src/main/java/io/openliberty/tools/eclipse/test/it/AbstractLibertyPluginSWTBotTest.java
+++ tests/src/main/java/io/openliberty/tools/eclipse/test/it/AbstractLibertyPluginSWTBotTest.java
@@ -22,12 +22,9 @@ import static io.openliberty.tools.eclipse.test.it.utils.SWTBotPluginOperations.
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 import org.eclipse.buildship.core.BuildConfiguration;
