86a70ef78d226b9a8ca876f1b493738c1c0a67db
Edward Mezarina <mezarina@us.ibm.com>
2026-06-22 10:47:10 -0400

    Unit test updates

--- tests/src/main/java/io/openliberty/tools/eclipse/test/ut/CommandBuilderTest.java
+++ tests/src/main/java/io/openliberty/tools/eclipse/test/ut/CommandBuilderTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright (c) 2025 IBM Corporation and others.
+* Copyright (c) 2025, 2026 IBM Corporation and others.
 *
 * This program and the accompanying materials are made available under the
 * terms of the Eclipse Public License v. 2.0 which is available at
@@ -15,15 +15,23 @@ package io.openliberty.tools.eclipse.test.ut;
 
 import static io.openliberty.tools.eclipse.test.it.utils.SWTBotPluginOperations.unsetBuildCmdPathInPreferences;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.io.File;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
 import org.junit.jupiter.api.Test;
 
 import io.openliberty.tools.eclipse.CommandBuilder;
+import io.openliberty.tools.eclipse.model.ProjectModel;
 
 public class CommandBuilderTest {
 
@@ -39,13 +47,14 @@ public class CommandBuilderTest {
         // resolve to "" + "/bin/mvn" = "/bin/mvn" and would typically be an actual path on Unix/Mac
         unsetBuildCmdPathInPreferences(new SWTWorkbenchBot(), "Maven");
         Path projectPath = Paths.get("resources", "applications", "maven", "liberty-maven-test-wrapper-app");
-        String retVal = CommandBuilder.getMavenCommandLine(projectPath.toString(), "-a 123", obfuscatedPath());
-        assertEquals((mvnwName()) + " -a 123", retVal, "Wrong cmd line");
+        ProjectModel projectModel = createMockProjectModel(projectPath);
+        String retVal = CommandBuilder.getMavenCommandLine(projectModel, "-a 123", obfuscatedPath());
+        assertEquals(projectPath.toAbsolutePath().resolve(wrapperName()) + " -a 123", retVal, "Wrong cmd line");
     }
 
     /**
      * Tests the CommandBuilder builds a mvn invocation using the mvn found in the path, even when there is an empty path element
-     * 
+     *
      * @throws Exception
      */
     @Test
@@ -53,7 +62,8 @@ public class CommandBuilderTest {
         Path projectPath = Paths.get("resources", "applications", "maven", "liberty-maven-test-app");
         Path mvnPath = Paths.get("resources", "execs");
         String pathEnv = obfuscatedPath() + File.pathSeparator + mvnPath.toAbsolutePath().toString();
-        String retVal = CommandBuilder.getMavenCommandLine(projectPath.toString(), "-a 123", pathEnv);
+        ProjectModel projectModel = createMockProjectModel(projectPath);
+        String retVal = CommandBuilder.getMavenCommandLine(projectModel, "-a 123", pathEnv);
         assertEquals(mvnPath.toAbsolutePath().resolve(mvnName()) + " -a 123", retVal, "Wrong cmd line");
     }
 
@@ -76,12 +86,42 @@ public class CommandBuilderTest {
         }
     }
 
-    private String mvnwName() {
+    private String wrapperName() {
         if (System.getProperty("os.name").contains("Windows")) {
-            return ".\\mvnw.cmd";
+            return "mvnw.cmd";
         } else {
-            return "./mvnw";
+            return "mvnw";
         }
+    }
+
+    /**
+     * Creates a mock ProjectModel for testing purposes
+     */
+    private ProjectModel createMockProjectModel(Path projectPath) throws Exception {
+        IProject mockProject = mock(IProject.class);
+        IPath mockIPath = mock(IPath.class);
+        IFile mockPomFile = mock(IFile.class);
+        IFile mockBuildGradleFile = mock(IFile.class);
+        IProjectDescription mockProjectDescription = mock(IProjectDescription.class);
+
+        when(mockProject.getLocation()).thenReturn(mockIPath);
+        when(mockIPath.toOSString()).thenReturn(projectPath.toAbsolutePath().toString());
+
+        // Mock getFile() to return mock IFile objects instead of null
+        when(mockProject.getFile("pom.xml")).thenReturn(mockPomFile);
+        when(mockProject.getFile("build.gradle")).thenReturn(mockBuildGradleFile);
+
+        // Mock the exists() method based on actual file existence
+        File pomFile = projectPath.resolve("pom.xml").toFile();
+        File buildGradleFile = projectPath.resolve("build.gradle").toFile();
+        when(mockPomFile.exists()).thenReturn(pomFile.exists());
+        when(mockBuildGradleFile.exists()).thenReturn(buildGradleFile.exists());
+
+        // Mock getDescription() to avoid NullPointerException
+        when(mockProject.getDescription()).thenReturn(mockProjectDescription);
+        when(mockProjectDescription.hasNature(anyString())).thenReturn(false);
+
+        return new ProjectModel(mockProject);
     }
 
 }
